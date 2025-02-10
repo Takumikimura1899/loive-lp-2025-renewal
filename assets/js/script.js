330aa10da719b528cb8c077321d3773f8d7118fe
Takumikimura1899 <kimura141899@gmail.com>
2025-02-10 11:57:20 +0900

    studio-list移植

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const studioButtons = document.querySelectorAll('.studio-button');
+  const studioInfoContainers = document.querySelectorAll('.studio-info-container');
+  const closeButtons = document.querySelectorAll('.close-button');
+
+  const initiallyActiveStudio = 'jiyugaoka';
+
+  studioButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      studioButtons.forEach((btn) => btn.classList.remove('active'));
+      button.classList.add('active');
+      studioInfoContainers.forEach((container) => (container.style.display = 'none'));
+
+      const targetContainerId = `studio-info-container-${button.dataset.studio}`;
+      const targetContainer = document.getElementById(targetContainerId);
+      if (targetContainer) {
+        targetContainer.style.display = 'block';
+      }
+    });
+
+    if (button.dataset.studio === initiallyActiveStudio) {
+      button.classList.add('active');
+    }
+  });
+
+  closeButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const targetContainerId = button.dataset.target;
+      const targetContainer = document.getElementById(targetContainerId);
+
+      if (targetContainer) {
+        targetContainer.style.display = 'none';
+      }
+
+      studioButtons.forEach((btn) => btn.classList.remove('active'));
+    });
+  });
+
+  const initiallyActiveContainer = document.getElementById(
+    `studio-info-container-${initiallyActiveStudio}`,
+  );
+  if (initiallyActiveContainer) {
+    initiallyActiveContainer.style.display = 'block';
+  }
+});
